db4fb58a631d347c0e95aee4b3daa124a79e3e53
Shim Jae Chun <shimnaldo98@gmail.com>
2024-03-29 13:48:00 +0900

    feat: add LayerManager

---
+++ Assets/ShimJaechun/01. Scripts/Manager/LayerManager.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LayerManager : Singleton<LayerManager> 
+{
+    [SerializeField]
+    public LayerMask wallLM;
+}
