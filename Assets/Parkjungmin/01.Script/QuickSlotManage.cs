22cbe5c7ae628d3936546b371079205424ba6298
ParkJungmin <vhzptvhdhfl@gmail.com>
2024-04-02 15:32:55 +0900

    feat 태양 프리팹

---
+++ Assets/Parkjungmin/01.Script/QuickSlotManage.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuickSlotManage : MonoBehaviour
+{
+	Item[] itemlist = new Item [6];
+
+
+	private void Start()
+	{
+		
+	}
+
+
+}
