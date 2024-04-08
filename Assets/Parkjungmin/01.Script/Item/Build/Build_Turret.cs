2e7f368359d8b267bfcbe562a993fc654f862983
ParkJungmin <vhzptvhdhfl@gmail.com>
2024-04-08 19:41:03 +0900

    feat : 포탑 기본기능

--- Assets/Parkjungmin/01.Script/Item/Build/Build_Turret.cs
+++ Assets/Parkjungmin/01.Script/Item/Build/Build_Turret.cs
@@ -7,20 +7,33 @@ using Jc;
 public class Build_Turret : MonoBehaviour
 {
     //공격할 타겟의 큐 -> 큐의 순서 = 공격 순위
-    Queue<Monster> monster_Queue = new Queue<Monster>(); //몬스터 큐 
-    Queue<Animal> animal_Queue= new Queue<Animal>(); //동물 큐
+     Queue<GameObject> monster_Queue = new Queue<GameObject>(); //몬스터 큐 
     /* 
      *  공격가능한 무언가가 범위에 들어왔다면 큐 삽입.
      *  큐에 들어온 순서대로 공격.
      *  타겟이 죽었거나, 범위를 벗어났을 경우 큐에서 제거.
      */
     [SerializeField] SphereCollider attack_Range;
+    [SerializeField] GameObject Turret_Head;
 
     [Header("터렛의 공격 거리 ")]
     [SerializeField] float range_Radius;
 
+    [Header("터렛의 공격 주기")]
+    [SerializeField] float attack_Per_Second;
+
+    Coroutine attakcoroutine;
+    bool IsAttack;
 
     Vector3 TargetDir;
+
+    IEnumerator AttackCoroutine()
+    {
+        //공격 주기만큼 대기.
+        yield return new WaitForSeconds(attack_Per_Second);
+        IsAttack = true;
+        
+    }
 
     private void OnEnable()
     {
@@ -36,41 +49,50 @@ public class Build_Turret : MonoBehaviour
     {
         monster_Queue = null;
     }
-    private void OnTriggerEnter(Collider other) // 범위 안에 들어왔을 경우 큐 삽입.
-    {
-        if (other.gameObject.layer == Manager.Layer.turretTargetableLM)
-        {
-            if (other.gameObject.GetComponent<Monster>() && !monster_Queue.Contains(other.gameObject.GetComponent<Monster>()))
-            {
-                monster_Queue.Enqueue(other.gameObject.GetComponent<Monster>());
-                Debug.Log($"몬스터 {other.gameObject.name}이 공격 순위 큐에 삽입되었습니다.");
-            }
-
-            else if (other.gameObject.GetComponent<Animal>() && !animal_Queue.Contains(other.gameObject.GetComponent<Animal>()))
-            {
-                monster_Queue.Enqueue(other.gameObject.GetComponent<Monster>());
-                Debug.Log($"동물 {other.gameObject.name}이 공격 순위 큐에 삽입되었습니다.");
-            }
-        }
-    }
     private void OnTriggerStay(Collider other) //범위 안에 있을 때는 공격 실행.
     {
-        //Debug.Log("1??");
-        //if (other.gameObject.layer == Manager.Layer.targetableLM)
-        //{
-        //    Debug.Log("??");
-        //    Turret_Attack();
-        //}
+
+        if (((1 << other.gameObject.layer) & (Manager.Layer.turretTargetableLM.value)) != 0)
+        {
+            Debug.Log("Enter");
+            if (!monster_Queue.Contains(other.gameObject))
+            {
+                monster_Queue.Enqueue(other.gameObject);
+                Debug.Log($"몬스터 {other.gameObject.name}이 공격 순위 큐에 삽입되었습니다.");
+            }
+        }
+        if (monster_Queue.Count > 0 )
+        {
+            AttackQueueCheck();
+        }
+        if (monster_Queue.Count <= 0)
+        {
+            Turret_Head.transform.forward = gameObject.transform.forward;
+        }
     }
     private void OnTriggerExit(Collider other) //범위를 벗어나면 큐 제거.
     {
-        monster_Queue.Dequeue();
+        if(monster_Queue.Contains(other.gameObject))
+        {
+            monster_Queue.Dequeue();
+            Debug.Log($"큐에서 몬스터 {other.gameObject.name} 제거됨");
+            
+        }
     }
-    void Turret_Attack() //큐에 넣은 순서대로 공격하는 로직.
+    void AttackQueueCheck() //큐에 넣은 순서대로 공격하는 로직.
     {
-        TargetDir = transform.position - monster_Queue.Peek().gameObject.transform.position;
-        Debug.Log("포탑이 공격중");
-        transform.forward = TargetDir;
+        TargetDir = (monster_Queue.Peek().gameObject.transform.position - Turret_Head.transform.position).normalized;
+        Turret_Head.transform.forward = TargetDir;
+
+        attakcoroutine = StartCoroutine(AttackCoroutine());
+        if (IsAttack)
+        {
+            Attack();
+        }
+    }
+    void Attack() //실제 공격
+    {
+        //공격 기능.
     }
 
 }
