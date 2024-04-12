e02b8e38b1411bca98da96d42e4c29b3598b6f9d
ParkJungmin <vhzptvhdhfl@gmail.com>
2024-04-12 20:41:06 +0900

    feat : 모닥불 구현

--- Assets/Parkjungmin/01.Script/Build/BuildSpawner.cs
+++ Assets/Parkjungmin/01.Script/Build/BuildSpawner.cs
@@ -13,6 +13,7 @@ namespace jungmin
         public Constructed_Turret turret3;
         public Constructed_Wall wall;
         public Constructed_Wall wall3;
+        public Constructed_Wall boneFire;
 
 
         private void Awake()
@@ -21,6 +22,7 @@ namespace jungmin
             Manager.Pool.CreatePool(turret3, turret.Size, turret.Size + 5);
             Manager.Pool.CreatePool(wall, wall.Size, wall.Size + 5);
             Manager.Pool.CreatePool(wall3, wall3.Size, wall3.Size + 5);
+            Manager.Pool.CreatePool(boneFire, boneFire.Size, boneFire.Size + 5);
         }
     }
 }
